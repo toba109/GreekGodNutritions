5a29c85a8aac455a21ab34e859d3763a238a7549
toba109 <tbalazs109@gmail.com>
2023-04-25 22:50:05 +0200

    jonaktunik

--- Bejelentkezes.php
+++ Bejelentkezes.php
@@ -1,90 +1,103 @@
 <?php
-    $db= new mysqli('localhost','root','','ggn');
-    if(isset($_POST['submit'])){
-        $errors = array();
-        $true = true;
-        if (empty($_POST['username'])){
-            $true = false;
-            array_push($errors, "A felhasználónév üres!");
-        }
-        if (empty($_POST['password'])){
-            $true = false;
-            array_push($errors, "A jelszó üres!");
-        }
-        if (empty($_POST['repassword'])){
-            $true = false;
-            array_push($errors, "A jelszó megerősítése üres!");
-        }
-        if (!($_POST['password']==$_POST['repassword'])){
-            $true=false;
-            array_push($errors,"A jelszavak nem egyenlőek!");
-        }
-        if($true){
-            //Regisztráció
+    $db_pass='Gk2U.c45qL5A7Yfv';
+    $db_user='ggn';
+
+        // Kapcsolódás az adatbázishoz
+    $conn = mysqli_connect("localhost", "$db_user", "$db_pass", "$db_user");
+
+    // Ellenőrizzük a kapcsolatot
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Ellenőrizzük, hogy az űrlap elküldésekor meg lett-e adva a felhasználónév és jelszó
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        // Ellenőrizzük, hogy a felhasználónév és jelszó helyes-e
+        $sql = "SELECT * FROM users WHERE username = '$username'";
+        $result = mysqli_query($conn, $sql);
+        
+        if (mysqli_num_rows($result) == 1) {
+            $row = mysqli_fetch_assoc($result);
+            if (password_verify($password, $row['password'])) {
+                // Ha a felhasználónév és jelszó helyesek, a felhasználó sikeresen bejelentkezett
+                session_start();
+                $_SESSION['username'] = $username;
+                header("Location: rendeles.php");
+                exit();
+            } else {
+                echo "Hibás felhasználónév vagy jelszó!";
+            }
+        } else {
+            echo "Hibás felhasználónév vagy jelszó!";
         }
     }
-    $db->close();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bejelentkezés</title>
+    <title>Regisztráció</title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="ikon/logo.png">
     <script src="https://kit.fontawesome.com/96ced10c0a.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/min/tiny-slider.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="css/bejelentkezes.css">
 </head>
 <body>
-    
     <nav class="navigacio">
         <input type="checkbox" id="check">
         <label for="check" class="checkfa">
             <i class="fa-solid fa-align-justify"></i>
         </label>
-        
+
         <label class="logo">GGN</label>
         <ul class="unorderedList">
             <li class="lista"><a href="index.html" target="_self">Főoldal</a></li>
             <li class="lista"><a href="Szolgáltatásaink.html" target="_self">Szolgáltatások</a></li>
-            <li class="lista"><a href="Elérhetőségeink.html" target="_self">Elérhetőség</a></li>
-            <li class="lista"><a href="Műveink.html" target="_self">Termékek</a></li>
+            <li class="lista"><a href="Termekeink.html" target="_self">Termékek</a></li>
             <li class="lista"><a class="active" href="Bejelentkezes.php" target="_self">Bejelentkezés</a></li>
+            <li class="lista"><a href="Regisztracio.php" target="_self">Regisztráció</a></li>
+            <li class="lista"><a href="rendeles.php" target="_self">Rendeléseim</a></li>
         </ul>
     </nav>
+    <section class="asd">
+       
+        <div class="wrapper">
             <div class="container">
-                <form action="Bejelentkezes.php" method="POST">
+                <p>Üdvözöllek a Bejelentkezési oldalon, kérlek add meg a regisztrációkor megadott adataid! </p><br>
+                <p>Ha még nem regisztráltál a Rágisztráció fülre kattintva tudod pótolni!</p><br>
+                <form action="" method="POST">
                     <div class="form-group">
                         <p class="nej" >Felhasználónév</p>
-                        <input name="username" type="text" class="form-control">
+                        <input name="username" id="username" type="text" class="form-control">
                     </div>
                     <div class="form-group">
                         <p class="nej">Jelszó</p>
-                        <input name="password" type="password" class="form-control">
+                        <input name="password" id="password" type="password" class="form-control">
                     </div>
-                    
-                    <div class="form-group">
-                        <p class="nej">Jelszó újra</p>
-                        <input name="repassword" type="password" class="form-control">
-                    </div>
-        
-                    <button class="btn btn-primary mt-3" type=submit name=submit>
-                        Mentés
+                    <button class="btn btn-primary mt-3">
+                        Bejelentkezés
                     </button>
                 </form>
-                <?php 
-                    if(!empty($errors)){
-                        foreach ($errors as $key) {
-                            echo $key."<br/>";
-                        }
-                    }
-                ?>
-            </div> 
+            </div>
+        </div> 
+    </section>  
+    <div class="copy">
+        <p class="null">Elérhetőség: </p>
+        <p class="null"><a href ="https://hu-hu.facebook.com/">Facebook</a>
+        <a href = "https://www.instagram.com/">Instagram</a>
+        <a href = "https://twitter.com/">Twitter</a>
+        <a href = https://www.youtube.com/watch?v=dQw4w9WgXcQ>Youtube</a></p>
+        <p>&copy Copyright 2023 Greek God Nutritions</p>       
+    </div>           
 </body>
 </html>
---
+++ Regisztracio.php
@@ -0,0 +1,103 @@
+<?php
+    $db_pass='Gk2U.c45qL5A7Yfv';
+    $db_user='ggn';
+
+            // Kapcsolódás az adatbázishoz
+    $conn = mysqli_connect("localhost", "$db_user", "$db_pass", "$db_user");
+
+    // Ellenőrizzük a kapcsolatot
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Ellenőrizzük, hogy az űrlap elküldésekor meg lett-e adva a felhasználónév és jelszó
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        
+        // Ellenőrizzük, hogy a felhasználónév még nem létezik az adatbázisban
+        $sql = "SELECT * FROM users WHERE username = '$username'";
+        $result = mysqli_query($conn, $sql);
+        
+        if (mysqli_num_rows($result) == 0) {
+            // Ha a felhasználónév még nem létezik, létrehozzuk a felhasználói fiókot
+            $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+            if (mysqli_query($conn, $sql)) {
+                echo "Sikeres regisztráció!";
+            } else {
+                echo "Hiba: " . mysqli_error($conn);
+            }
+        } else {
+            echo "A megadott felhasználónév már létezik!";
+        }
+    }
+    
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regisztráció</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="ikon/logo.png">
+    <script src="https://kit.fontawesome.com/96ced10c0a.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/min/tiny-slider.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css">
+    <link rel="stylesheet" href="css/bejelentkezes.css">
+    <link rel="stylesheet" href="css/arak_eszkozok.css">
+</head>
+<body>
+
+    <nav class="navigacio">
+        <input type="checkbox" id="check">
+        <label for="check" class="checkfa">
+            <i class="fa-solid fa-align-justify"></i>
+        </label>
+        
+        <label class="logo">GGN</label>
+        <ul class="unorderedList">
+            <li class="lista"><a href="index.html" target="_self">Főoldal</a></li>
+            <li class="lista"><a href="Szolgáltatásaink.html" target="_self">Szolgáltatások</a></li>
+            <li class="lista"><a href="Termekeink.html" target="_self">Termékek</a></li>
+            <li class="lista"><a href="Bejelentkezes.php" target="_self">Bejelentkezés</a></li>
+            <li class="lista"><a class="active" href="Regisztracio.php" target="_self">Regisztráció</a></li>
+            <li class="lista"><a href="rendeles.php" target="_self">Rendeléseim</a></li>
+        </ul>
+    </nav>
+
+  
+    <section class="asd">
+       
+        <div class="wrapper">
+            <div class="container">
+                <p>Üdvözöllek a Regisztrációs oldalon, kérlek adj meg egy felhasználónevet és egy jelszót!</p><br>
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <p class="nej" >Felhasználónév</p>
+                        <input name="username" id="username" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <p class="nej">Jelszó</p>
+                        <input name="password" id="password" type="password" class="form-control">
+                    </div>
+                    <button class="btn btn-primary mt-3">
+                        Regisztráció
+                    </button>
+                </form>
+            </div>
+        </div> 
+    </section>                    
+    <div class="copy">
+        <p class="null">Elérhetőség: </p>
+        <p class="null"><a href ="https://hu-hu.facebook.com/">Facebook</a>
+        <a href = "https://www.instagram.com/">Instagram</a>
+        <a href = "https://twitter.com/">Twitter</a>
+        <a href = https://www.youtube.com/watch?v=dQw4w9WgXcQ>Youtube</a></p>
+        <p>&copy Copyright 2023 Greek God Nutritions</p>       
+    </div>   
+</body>
+</html>
